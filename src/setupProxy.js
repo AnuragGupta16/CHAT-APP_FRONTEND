63caac6d28556dfbbd9bf6c8692471e271584352
Anurag Gupta <86673132+AnuragGupta16@users.noreply.github.com>
2023-01-21 06:09:12 +0530

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:5000',
+      target: 'https://wonderful-meerkat-de194d.netlify.app',
       changeOrigin: true,
     })
   );
